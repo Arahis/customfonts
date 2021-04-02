39efc61b4a88f54c67cc01cc126dad046fe076c6
Daria Brynza <dahis.br@gmail.com>
2021-04-02 22:46:16 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">ROBOROCKS</div>;
+  return (
+    <div className="App">
+      <h1>ROBOROCKS</h1>
+      <h4>I love robots</h4>
+    </div>
+  );
 }
 
 export default App;
